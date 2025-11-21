951fd2fcbe5913156e595d78cd2ed35d8a9cbf7c
Janet Blackquill <uhhadd@gmail.com>
2025-11-21 00:32:28 -0500

    rebase

--- Content.Server/Zombies/ZombieSystem.cs
+++ Content.Server/Zombies/ZombieSystem.cs
@@ -259,6 +259,14 @@ namespace Content.Server.Zombies
                     args.Handled = true;
                     continue;
                 }
+                else if (!HasComp<WoundableComponent>(uid)) // Offbrand
+                {
+                    if (!HasComp<ZombieImmuneComponent>(uid) && !cannotSpread && _random.Prob(GetZombieInfectionChance(uid, entity.Comp)))
+                    {
+                        EnsureComp<PendingZombieComponent>(uid);
+                        EnsureComp<ZombifyOnDeathComponent>(uid);
+                    }
+                }
 
                 if (_mobState.IsAlive(uid, mobState))
                 {
--- Content.Shared/Chemistry/Reagent/ReagentPrototype.cs
+++ Content.Shared/Chemistry/Reagent/ReagentPrototype.cs
@@ -304,24 +304,10 @@ namespace Content.Shared.Chemistry.Reagent
     public sealed partial class ReagentStatusEffectEntry
     {
         [DataField]
-        public EntityEffectCondition[]? Conditions;
+        public Content.Shared.EntityConditions.EntityCondition[]? Conditions;
 
         [DataField]
         public EntProtoId StatusEffect;
-
-        public bool ShouldApplyStatusEffect(EntityEffectBaseArgs args)
-        {
-            if (Conditions != null)
-            {
-                foreach (var cond in Conditions)
-                {
-                    if (!cond.Condition(args))
-                        return false;
-                }
-            }
-
-            return true;
-        }
 
         public string? Describe(IPrototypeManager prototype, IEntitySystemManager entSys)
         {
@@ -331,7 +317,7 @@ namespace Content.Shared.Chemistry.Reagent
             return Loc.GetString("reagent-guidebook-status-effect", ("effect", effectProtoData.Name ?? string.Empty),
                 ("conditionCount", Conditions?.Length ?? 0),
                 ("conditions",
-                    Content.Shared.Localizations.ContentLocalizationManager.FormatList(Conditions?.Select(x => x.GuidebookExplanation(prototype)).ToList() ??
+                    Content.Shared.Localizations.ContentLocalizationManager.FormatList(Conditions?.Select(x => x.EntityConditionGuidebookText(prototype)).ToList() ??
                                                             new List<string>())));
         }
     }
